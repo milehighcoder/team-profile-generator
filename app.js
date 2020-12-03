7553c64ca934d145927bed6c0e7a00fe8f175f93
mykereel <mgmartnz@icloud.com>
2020-12-02 19:50:32 -0700

    classes created and tests passed

--- app.js
+++ app.js
@@ -10,9 +10,22 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the name of your Manager?',
+            name: 'managerName',
+        }
+
+
+
+
+
+    ])
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
